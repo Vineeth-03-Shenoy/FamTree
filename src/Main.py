f745c5c113e581cfd7170f60162557aa473be61d
Vineeth-03-Shenoy <vineethshenoy22@gmail.com>
2022-11-21 17:50:29 +0530

    Updated main method

--- src/Main.py
+++ src/Main.py
@@ -1,3 +1,12 @@
+import dbConnection
 import FamMember
 
-fname = input("Enter Name: ")
+def main():
+    dbConnection.connection()
+    dbConnection.DBcreate()
+    FamMember.fam_MemberTable()
+    FamMember.FM_Insert()
+    
+
+if __name__ =='__main__':
+    main()
