2b068c6531bc8a1927fb38b38151f504aa6c64f2
Vineeth-03-Shenoy <vineethshenoy22@gmail.com>
2022-11-28 11:45:49 +0530

    Updated with DB details

--- FamDBMS/settings.py
+++ FamDBMS/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'FamDBMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'famtree',
+        'USER': 'root',
+        'PASSWORD':'VineethMYSQL@123',
+        'HOST': '127.0.0.1',
+        'PORT':'3307',
+        'OPTIONS':{
+            'init_command':"SET sql_mode='STRICT_TRANS_TABLES"
+        }
     }
 }
 
