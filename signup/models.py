f548896600ea0061539ff49455f59511d09011ec
Vineeth-03-Shenoy <vineethshenoy22@gmail.com>
2022-12-02 18:49:02 +0530

    updated

--- signup/models.py
+++ signup/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Family_Member(models.Model):
+    member_ID=models.CharField(max_length=20)
+    FName=models.CharField(max_length=20)
+    Name=models.CharField(max_length=25)
+    LName=models.CharField(max_length=20)
+    DoB=models.DateField()
+    registered=models.DateTimeField(auto_now=True)
+    DoD=models.DateField()
