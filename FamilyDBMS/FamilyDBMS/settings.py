044cf428e791b10b6b84d6f76a81d0cdb8fd44c4
Vineeth Shenoy P <20ai059.vineeth@sjec.ac.in>
2022-12-08 06:31:08 +0530

    'updated  with database details

--- FamilyDBMS/FamilyDBMS/settings.py
+++ FamilyDBMS/FamilyDBMS/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'famapp',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,11 @@ WSGI_APPLICATION = 'FamilyDBMS.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'familytesttree',
+        'USER':'root',
+        'PASSWORD': 'VineethMYSQL@123',
+        'HOST':'localhost',
+        'PORT':'3307',
     }
 }
 
