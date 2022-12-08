af0b918d2e1658261c6be58d31b87d730a2e88d1
Vineeth Shenoy P <20ai059.vineeth@sjec.ac.in>
2022-12-08 07:23:28 +0530

    updates

--- FamilyDBMS/famapp/views.py
+++ FamilyDBMS/famapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def hello(requets):
+    text="""<h1>welcome to my famapp !</h1>"""
+    return HttpResponse(text)
